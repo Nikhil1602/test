198a7bdd7bce9c5bb2739f9210d7e07b0487ce87
Nikhil1602 <nikhilbarot1602@gmail.com>
2026-03-25 09:41:18 +0530

    routes-created-in-different-folder

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require('express');
+const orderRoutes = require("./routes/orders");
+const productRoutes = require("./routes/products");
 
 const app = express();
 
-const middleware = (req, res, next) => {
-    console.log(`${req.method} request made to ${req.url}`);
-    next();
-}
-
-app.get("/products", middleware, (req, res) => {
-    res.send("Here is the list of all products.");
-});
-
-app.post("/products", middleware, (req, res) => {
-    res.send("A new product has been added.");
-});
-
-app.get("/categories", middleware, (req, res) => {
-    res.send("Here is the list of all categories.");
-});
-
-app.post("/categories", middleware, (req, res) => {
-    res.send("A new category has been created.");
-});
+app.use("/orders", orderRoutes);
+app.use("/products", productRoutes);
 
 // Start server
 app.listen(4000, () => {
