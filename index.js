83c2f6b78f3a643bef4399194f6535b3f075ef26
Nikhil1602 <nikhilbarot1602@gmail.com>
2026-03-25 09:03:57 +0530

    middleware-added-for-req-method-and-req-url

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/welcome/:username', (req, res) => {
-    const { username } = req.params;
-    const { role } = req.query;
+const middleware = (req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+}
 
-    res.send(`Welcome ${username}, your role is ${role}`);
+app.get("/products", middleware, (req, res) => {
+    res.send("Here is the list of all products.");
+});
+
+app.post("/products", middleware, (req, res) => {
+    res.send("A new product has been added.");
+});
+
+app.get("/categories", middleware, (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+app.post("/categories", middleware, (req, res) => {
+    res.send("A new category has been created.");
 });
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
 });
