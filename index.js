1e102f481a17034987454b5912c1cb2f0e97d65d
Nikhil1602 <nikhilbarot1602@gmail.com>
2026-03-25 08:49:12 +0530

    query-and-params

--- index.js
+++ index.js
@@ -2,49 +2,14 @@ const express = require('express');
 
 const app = express();
 
-// Middleware to parse JSON (useful for POST requests)
-app.use(express.json());
+app.get('/welcome/:username', (req, res) => {
+    const { username } = req.params;
+    const { role } = req.query;
 
-// GET /products
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products.');
-});
-
-// POST /products
-app.post('/products', (req, res) => {
-    res.send('A new product has been added.');
-});
-
-// GET /categories
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all categories.');
-});
-
-// POST /categories
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created.');
-});
-
-app.use((req, res) => {
-    res.status(404).send("<h1>404 - Page Not Found</h1>");
+    res.send(`Welcome ${username}, your role is ${role}`);
 });
 
 // Start server
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
-
-
-// Deliverables:
-
-// Build an Express server that listens on port 4000 and handles the following endpoints:
-// GET /products - Respond with "Here is the list of all products."
-// POST /products - Respond with "A new product has been added."
-// GET /categories - Respond with "Here is the list of all categories."
-// POST /categories - Respond with "A new category has been created."
-// Additional Requirements:
-// Wildcard Route:
-// Use a wildcard route (*) to handle all undefined routes and return the following custom error page as HTML:<h1>404 - Page Not Found</h1>
-// Also add the status as 404.
-// Test all valid routes (GET and POST).
-// Access an undefined route, like /random, and verify that the custom error page is displayed.
